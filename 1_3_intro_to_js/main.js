2f9d5c8b3a197e1d0ed2abc1b8fce317fd7c7150
jinnic <yoohyojin@gmail.com>
2024-02-13 10:49:18 -0500

    Assignment Intro to JS

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const btn = document.querySelector('.btn')
+const counter = document.querySelector('#counter')
+const x = document.querySelector('#x')
+const y = document.querySelector('#y')
+
+let count = 0;
+
+btn.addEventListener('click', (event)=>{
+  let canvas = btn.getBoundingClientRect(); 
+  let positionX = event.clientX - canvas.left;
+  let positionY = event.clientY - canvas.top
+  count += 1;
+  counter.textContent = count;
+  x.textContent = positionX;
+  y.textContent = positionY;
+  // console.log('Cursor position: ' + positionX + ',' + positionY)
+})
+
+
