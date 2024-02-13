185c929acd0522dd57b596deba69a38205bf61c5
jinnic <yoohyojin@gmail.com>
2024-02-13 11:06:43 -0500

    add storing array of objects

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,19 +1,31 @@
-const btn = document.querySelector('.btn')
-const counter = document.querySelector('#counter')
-const x = document.querySelector('#x')
-const y = document.querySelector('#y')
+const btn = document.querySelector('.btn');
+const counter = document.querySelector('#counter');
+const x = document.querySelector('#x');
+const y = document.querySelector('#y');
 
+let clickCoordinates = [];
 let count = 0;
 
 btn.addEventListener('click', (event)=>{
   let canvas = btn.getBoundingClientRect(); 
   let positionX = event.clientX - canvas.left;
   let positionY = event.clientY - canvas.top
+  let currentCoordinate = {
+    x: 0,
+    y: 0
+  };
+  
   count += 1;
   counter.textContent = count;
   x.textContent = positionX;
   y.textContent = positionY;
   // console.log('Cursor position: ' + positionX + ',' + positionY)
+
+  currentCoordinate.x = positionX;
+  currentCoordinate.y = positionY;
+  clickCoordinates.push(currentCoordinate);
+
+  // console.log(clickCoordinates)
 })
 
 
