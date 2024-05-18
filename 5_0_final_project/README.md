9b3759f44c29d2ed5dd2769bf76e7f362ffb7d27
jinnic <yoohyojin@gmail.com>
2024-05-18 15:48:52 -0400

    complete horizontal graph

---
+++ 5_0_final_project/README.md
@@ -0,0 +1,17 @@
+# Final project
+The goals for this tutorial are:
+
+- Creating a data visualization that gives your users the ability to explore or understand the MoMA collection dataset of artwork and artists. 
+- The data can be found on github here: [https://github.com/MuseumofModernArt/collection](https://github.com/MuseumofModernArt/collection.).
+
+## Deliverables 
+Prospectus: cleaned and organized notes after ideation class session. 
+Sketches: cleaned and organized drawings after prototyping class session.
+Final project: a live, working site with data visualizations that satisfy the project requirements.
+
+## Requirements
+MoMA dataset is leveraged in some capacity. All data presented should be factual (as well as any analysis or manipulations), and data sources should be referenced.
+
+The page is live, accessible, and the visualization loads properly. The page should include other aspects of a webpage, such as a heading, context, basic structure, etc. 
+
+The webpage uses d3.js and/or mapbox for one or more visualizations. You can use any of the tutorial visualizations or expand to other uses of d3.js. You may include other visualization libraries if it helps articulating your research, but you must use d3.js or mapbox in some capacity.
--- 5_2_horizontal_stacked_graph/main.js
+++ 5_2_horizontal_stacked_graph/main.js
@@ -6,23 +6,39 @@
 
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.8;
-const height = 450;
 const margin = 70;
 
 /* LOAD DATA */
 d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
   .then(data => {
     
-  // List of country: remove year column which is 0 index
-  const countries = data.columns.slice(1)
+    // List of country: remove year column which is 0 index
+    const countries = data.columns.slice(1)
 
-  // List of years: map the value of the first column called Year for X axis
-  const years = data.map(d => (d.Year))
-  console.log("data", data)
-  console.log("countries", countries)
-  console.log("years", years)
+    // List of years: map the value of the first column called Year for X axis
+    const years = data.map(d => (d.Year))
+    console.log("data", data)
+    console.log("countries", countries)
+    console.log("years", years)
 
+    /** stacked data */
+    const stackedData = d3.stack()
+    .keys(countries)
+    .order(d3.stackOrderDescending)
+    (data)
 
+    const stackedDataNormal = d3.stack()
+      .keys(countries)
+      .order(d3.stackOrderDescending)
+      .offset(d3.stackOffsetExpand)
+      (data)
+    //console.log("stackedData", stackedData)
+    //console.log("stackedDataNormal", stackedDataNormal)
+    
+    // Compute the height from the number of stacks.
+    const height = stackedData.length * 25 + margin*2;
+    // console.log("height", height)
+      
     /** select/create SVG */
     const barChartVertical = d3.select("#container")
       .append("svg")
@@ -37,6 +53,8 @@ d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
       .attr("width", width)
       .attr("height", height)
       .style("overflow", "visible");
+    
+
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
     
@@ -44,59 +62,44 @@ d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
     * barChartVertical
     */
 
-    /** stacked data */
-    const stackedData = d3.stack()
-      .keys(countries)
-      .order(d3.stackOrderDescending)
-      (data)
-
-    const stackedDataNormal = d3.stack()
-      .keys(countries)
-      .order(d3.stackOrderDescending)
-      .offset(d3.stackOffsetExpand)
-      (data)
-
-    /** y axis for Stacked bar chart - count of works */
-    const yScale = d3.scaleLinear()
+    /** x axis for Stacked bar chart - count of works */
+    const xScale = d3.scaleLinear()
       .domain([0, 1500])
-      .range([height - margin, margin/3])
-
-    const yAxis = d3.axisLeft(yScale)
-        
-    barChartVertical.append("g")
-      .attr("transform", `translate(${margin}, 0)`)
-      .call(yAxis)   
-
-    /** y axis for 100% Stacked bar chart, normalized - % of works */
-    const yScaleNormal = d3.scaleLinear()
-      .domain([0, d3.max(stackedDataNormal, d => d3.max(d, d =>{ console.log("MAX",d, d[1]); return d[1]}))])
-      .range([height - margin, margin/3])
-    
-    const yAxisNormal = d3.axisLeft(yScaleNormal).ticks(height/50, "%")
-    
-    barChartVerticalNormal.append("g")
-      .attr("transform", `translate(${margin}, 0)`)
-      .call(yAxisNormal)
-    
-    const xScale = d3.scaleBand()
-      .domain(years)
       .range([margin, width - margin])
+
+    const xAxis = d3.axisBottom(xScale).tickSizeOuter(0) 
+
+    /** x axis for 100% Stacked bar chart, normalized - % of works */
+    const xScaleNormal = d3.scaleLinear()
+      .domain([0, d3.max(stackedDataNormal, d => d3.max(d, d =>{ 
+        //console.log("MAX",d, d[1]);
+        return d[1]
+      }))])
+      .range([margin, width - margin])
+    
+    const xAxisNormal = d3.axisBottom(xScaleNormal).ticks(width/100, "%")
+      
+    const yScale = d3.scaleBand()
+      .domain(years)
+      .range([height - margin, margin/3])
       .paddingInner(0.2)
       .paddingOuter(0.3)
     
     /** x axis for both - year */
-    const xAxis = d3.axisBottom(xScale).tickSizeOuter(0)
+    const yAxis = d3.axisLeft(yScale)
 
-    barChartVertical.append("g")
-      .attr("transform", `translate(0, ${height - margin})`)
-      .call(xAxis)
-
-    barChartVerticalNormal.append("g")
-      .attr("transform", `translate(0, ${height - margin})`)
-      .call(xAxis)
+    /** function to draw axis - to prevent axis being burried back */
+    const drawAxis = (svg, xAxis, yAxis) => {
       
+      svg.append("g")
+        .attr("transform", `translate(${margin}, 0)`)
+        .call(yAxis)
 
-   
+      svg.append("g")
+        .attr("transform", `translate(0, ${height - margin})`)
+        .call(xAxis)
+    }
+    
     /** Color scale */
     const colorScale = d3.scaleOrdinal(d3.schemePaired)
       .domain(countries)
@@ -138,13 +141,10 @@ d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
     /** Stacked bar chart*/
     barChartVertical.selectAll(".bar")
     // Enter in the stack data = loop key per key = group per group
-    .data(d => {
-      //console.log("stackedData", stackedData)
-      return stackedData
-    })
+    .data(d => stackedData)
     .join("g")
       .attr("class", "bar")
-      .attr("fill",d => colorScale(d.key))
+      .attr("fill", d => colorScale(d.key))
       .attr("stroke", "grey")
       .selectAll("rect")
       // enter a second time = loop subgroup per subgroup to add all rectangles
@@ -152,10 +152,10 @@ d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
         //console.log("data", d)
         return d})
       .join("rect")
-        .attr("x", d => xScale(d.data.Year))
-        .attr("y", d => yScale(d[1]))
-        .attr("height", d => yScale(d[0]) - yScale(d[1]))
-        .attr("width",xScale.bandwidth())
+        .attr("x", d => xScale(d[0]))
+        .attr("y", d => yScale(d.data.Year))
+        .attr("height", yScale.bandwidth())
+        .attr("width", d => xScale(d[1]) - xScale(d[0]))
         .on("mouseover", mouseover)
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
@@ -165,7 +165,7 @@ d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
       .attr("text-anchor", "end")
       .attr("x", width - margin)
       .attr("y", height - margin/2)
-      .text("Nationality");
+      .text("Count");
 
     //Y axis label
     barChartVertical.append("text")
@@ -173,29 +173,32 @@ d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
       .attr("transform", "rotate(-90)")
       .attr("x", -margin/3 )
       .attr("y", 20 )
-      .text("Count");
+      .text("Nationality");
       
+    
     /** 100% Stacked bar chart, normalized */
+
     barChartVerticalNormal.selectAll(".bar")
-    // Enter in the stack data = loop key per key = group per group
     .data(d => {
       //console.log("stackedDataNormal", stackedDataNormal)
       return stackedDataNormal
     })
     .join("g")
       .attr("class", "bar")
+      .attr("z-index", -100)
       .attr("fill",d => colorScale(d.key))
       .attr("stroke", "grey")
       .selectAll("rect")
       // enter a second time = loop subgroup per subgroup to add all rectangles
       .data(d => {
-        //console.log("data", d)
+        //console.log("data", d[0])
         return d})
       .join("rect")
-        .attr("x", d => xScale(d.data.Year))
-        .attr("y", d => yScaleNormal(d[1]))
-        .attr("height", d => yScaleNormal(d[0]) - yScaleNormal(d[1]))
-        .attr("width",xScale.bandwidth())
+        .attr("x", d => xScaleNormal(d[0]))
+        .attr("y", d => yScale(d.data.Year))
+        .attr("height", yScale.bandwidth())
+
+        .attr("width", d => xScaleNormal(d[1]) - xScaleNormal(d[0]))
         .on("mouseover", mouseover)
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
@@ -205,7 +208,7 @@ d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
       .attr("text-anchor", "end")
       .attr("x", width - margin)
       .attr("y", height - margin/2)
-      .text("Nationality");
+      .text("Count");
 
     //Y axis label
     barChartVerticalNormal.append("text")
@@ -213,8 +216,12 @@ d3.csv('../data/MoMA-yearly-sample.csv', d3.autoType)
       .attr("transform", "rotate(-90)")
       .attr("x", -margin/3 )
       .attr("y", 20 )
-      .text("Count");
+      .text("Nationality");
 
+    /** Draw Axis */
+    drawAxis(barChartVertical,xAxis,yAxis)
+    drawAxis(barChartVerticalNormal,xAxisNormal,yAxis)
     
 
   })
+
